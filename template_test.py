e7643709f99cb27f53e6a39e557dbeba6c06921d
Dan Simon <dan@pipeline.com>
2017-12-29 22:03:03 -0500

    template can now take grammars

--- template_test.py
+++ template_test.py
@@ -1,8 +1,12 @@
 from mini_parse import Grammar, string_grammar, template, digit, letter, exact
 
-template_grammmar = Grammar(string_grammar)
+template_grammar = Grammar(string_grammar)
 
-template_grammmar.main = template(
-    '{a:digit} {b:dragons} {!:id}; {!:id} {c:people} {!:id}',
-    {'digit': digit, 'id': (letter | exact(' ')).plus().concat_all(),
-    'people': letter.plus().concat_all().join(exact(' and '), 2)})
+template_grammar.digit = digit
+
+template_grammar.id = (letter | exact(' ')).plus().concat_all()
+
+template_grammar.people = letter.plus().concat_all().join(exact(' and '), 2)
+
+template_grammar.main = template(
+    '{a:digit} {b:dragons} {!:id}; {!:id} {c:people} {!:id}', template_grammar)
